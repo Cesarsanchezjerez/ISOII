fc0a8bbd8ac0b40bca75e4ae9feaea489ccb650f
ManuelPuente <43786852+ManuelPuente@users.noreply.github.com>
2018-10-09 12:18:26 +0200

    el bicho es un violador

--- src/Manu.java
+++ src/Manu.java
@@ -2,7 +2,7 @@
 public class Manu {
 	public static void main(String args[]) {
 	System.out.println("HOLA CHAVALES SOY MANU");
-	System.out.println("HOLA");
+	System.out.println("el bicho");
 	}
 
 }
