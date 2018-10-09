79227937987ee3070b96438c1c0508de0d4943eb
AlexisTarjuelo <Alexis.Tarjuelo@alu.uclm.es>
2018-10-09 12:04:40 +0200

    Cambio realizado

--- src/ydtm.java
+++ src/ydtm.java
@@ -2,5 +2,7 @@
 public class ydtm {
 	public static void main(String[] args) {
 		System.out.println("Hola soy el puto amo");
+		System.out.println("MODIFICACION MANU");
+		System.out.println("SHUUUUUUU");
 	}
 }
