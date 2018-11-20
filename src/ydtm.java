191eade78a42321ad2a9727cdefd4f263b87fbc3
Jaimej5 <43786797+Jaimej5@users.noreply.github.com>
2018-11-20 11:43:22 +0100

    df

--- src/ydtm.java
+++ src/ydtm.java
@@ -1,6 +1,6 @@
 
 public class ydtm {
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		System.out.println("Hola soy el puto amo");
 		System.out.println("MODIFICACION MANU");
 		System.out.println("SHUUUUUUU");
