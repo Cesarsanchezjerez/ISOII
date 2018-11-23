c19af76905575ef462fb68ce7683d0859c6ee510
Cesarsanchezjerez <32263955+Cesarsanchezjerez@users.noreply.github.com>
2018-11-23 01:16:27 +0100

    Add files via upload
    seleccionar mesa

---
+++ iteracion5/GestorComandas.java
@@ -0,0 +1,19 @@
+package Codigo1;
+import java.util.ArrayList;
+public class GestorComandas {
+
+	private ArrayList<Mesa> mesas = new ArrayList<Mesa>();
+
+	public void SeleccionarMesa() {
+		// TODO - implement GestorComandas.SeleccionarMesa
+		throw new UnsupportedOperationException();
+	}
+
+	public void TomarComanda() {
+		// TODO - implement GestorComandas.TomarComanda
+		throw new UnsupportedOperationException();
+	}
+
+	
+
+}
