96ec7e7928d955663c2a65a5a03e84dbd068f29d
Cesarsanchezjerez <32263955+Cesarsanchezjerez@users.noreply.github.com>
2018-11-23 01:08:29 +0100

    Delete ydtm.java

--- src/ydtm.java
+++
@@ -1,8 +0,0 @@
-
-public class ydtm {
-	public static void main(String[] args){
-		System.out.println("Hola soy el puto amo");
-		System.out.println("MODIFICACION MANU");
-		System.out.println("SHUUUUUUU");
-	}
-}
