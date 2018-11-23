41f749e74ca72c375cc3dc7f34c03153fa302e97
Cesarsanchezjerez <32263955+Cesarsanchezjerez@users.noreply.github.com>
2018-11-23 01:26:00 +0100

    Add files via upload
    notificar bebida

---
+++ iteracion14/Administrador.java
@@ -0,0 +1,9 @@
+package Codigo1;
+public class Administrador extends Usuario {
+
+	void AccederEstadisticas() {
+		// TODO - implement Administrador.AccederEstadisticas
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ iteracion14/GesstorEstadisticas.java
@@ -0,0 +1,11 @@
+package Codigo1;
+public class GesstorEstadisticas {
+
+	
+
+	public void CalcularEstadisticas() {
+		// TODO - implement GesstorEstadisticas.CalcularEstadisticas
+		throw new UnsupportedOperationException();
+	}
+
+}
